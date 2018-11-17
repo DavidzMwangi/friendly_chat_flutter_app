d109ab891e658e4fddc8c2ab0f57ef8b431bc25f
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-11-17 14:50:53 +0300

    adding message

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // Replace the code in main.dart with the following.
 
 import 'package:flutter/material.dart';
+const String _name = "David";
 
 void main() {
   runApp(new FriendlychatApp());
@@ -23,6 +24,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen>{
+  final List<ChatMessage> _messages = <ChatMessage>[];             // new
+
   final TextEditingController _textController = new TextEditingController(); //new
 
   @override
@@ -30,7 +33,29 @@ class ChatScreenState extends State<ChatScreen>{
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendly Chat"),
       ),
-      body: _buildTextComposer(),
+      body:new Column(
+
+        children: <Widget>[                                         //new
+          new Flexible(                                             //new
+            child: new ListView.builder(                            //new
+              padding: new EdgeInsets.all(8.0),                     //new
+              reverse: true,                                        //new
+              itemBuilder: (_, int index) => _messages[index],      //new
+              itemCount: _messages.length,                          //new
+            ),                                                      //new
+          ),                                                        //new
+          new Divider(height: 1.0),                                 //new
+          new Container(                                            //new
+            decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),                  //new
+            child: _buildTextComposer(),                       //modified
+          ),                                                        //new
+        ],                                                          //new
+
+
+
+
+    ),
     );
   }
 
@@ -65,8 +90,49 @@ class ChatScreenState extends State<ChatScreen>{
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(                         //new
+      text: text,                                                  //new
+    );                                                             //new
+    setState(() {                                                  //new
+      _messages.insert(0, message);                                //new
+    });
   }
 }
+
+
+class ChatMessage extends StatelessWidget{
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0]),),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name,style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top:5.0),
+                child: new Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
 
 
 
