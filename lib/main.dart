25f20452cd405f0f83aa4b4e8a74b005db03dc1c
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-11-17 14:24:38 +0300

    completed create new message module

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,79 @@
- import 'package:flutter/material.dart';
+// Replace the code in main.dart with the following.
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendly Chat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+    @override
+  State createState()=> new ChatScreenState();
+
+}
+
+class ChatScreenState extends State<ChatScreen>{
+  final TextEditingController _textController = new TextEditingController(); //new
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendly Chat"),
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+
+  Widget _buildTextComposer() {
+    return new IconTheme(                                            //new
+      data: new IconThemeData(color: Theme.of(context).accentColor), //new
+      child: new Container(                                     //modified
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),                                                             //new
+    );
+  }
+
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+}
+
+
+
+
+
+
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
